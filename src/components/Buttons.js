ac5ea7f0df92987ba8941630f9cc416ec54b5b19
Cody Nicholson <cody.nicholson@Perficient.com>
2018-07-13 14:01:27 -0500

    init commit

---
+++ src/components/Buttons.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Buttons extends Component {
+    render() {
+      return (
+        <div>
+            <a className="downloadBtnL" href="https://drive.google.com/file/d/1ZCtGId37TZ4yGHPIeZ9BadQWs69WdR7H/view?usp=sharing" target="_blank" rel="noopener noreferrer">Download Résumé</a>
+            <a className="downloadBtnS" href="https://drive.google.com/file/d/1ZCtGId37TZ4yGHPIeZ9BadQWs69WdR7H/view?usp=sharing" target="_blank" rel="noopener noreferrer">↧</a>
+            {/* <a className="returnHomeL" href="">Return Home</a>
+            <a className="returnHomeS" href="">&#x21A9;</a> */}
+        </div>
+      );
+    }
+}
+  
+export default Buttons;
+  
